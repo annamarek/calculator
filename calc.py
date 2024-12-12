9041323d5a61e041af45d80aa3669827d47e4c5e
Anna Stoliarova <anastolarova61@gmail.com>
2024-12-12 14:09:10 +0300

    Added a multiplication method

--- calc.py
+++ calc.py
@@ -7,7 +7,10 @@ class Calculator:
         return f"{self._a} + {self._b} = {self._a+self._b}"
     def Subtraction(self):
         return f"{self._a} - {self._b} = {self._a-self._b}"
+    def Multiplication(self):
+        return f"{self._a} * {self._b} = {self._a*self._b}"
 
 c = Calculator(3,5)
 print(c.Addition())
 print(c.Subtraction())
+print(c.Multiplication())
